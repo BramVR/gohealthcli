5e211cb4bf88ee91b19777a9adc2bea5d68c4eec
Bram Van Rompuy <bram.van.rompuy@gmail.com>
2026-06-12 08:55:37 +0200

    refactor(cli): move owner-only filesystem helpers into owner_only_fs.go (#285)

--- cmd/gohealthcli/main.go
+++ cmd/gohealthcli/main.go
@@ -2264,44 +2264,6 @@ func parseCommaList(value string) []string {
 	return out
 }
 
-func ensureOwnerOnlyDir(dir string) error {
-	if info, err := os.Stat(dir); err == nil {
-		return validateOwnerOnlyDirInfo(dir, info)
-	} else if !os.IsNotExist(err) {
-		return err
-	}
-	if err := os.MkdirAll(dir, 0o700); err != nil {
-		return err
-	}
-	if !usesPOSIXPermissions() {
-		return nil
-	}
-	return os.Chmod(dir, 0o700)
-}
-
-func validateOwnerOnlyDir(dir string) error {
-	info, err := os.Stat(dir)
-	if err != nil {
-		return err
-	}
-	return validateOwnerOnlyDirInfo(dir, info)
-}
-
-func validateOwnerOnlyDirInfo(dir string, info os.FileInfo) error {
-	if !info.IsDir() {
-		return fmt.Errorf("%s is not a directory", dir)
-	}
-	if usesPOSIXPermissions() && info.Mode().Perm() != 0o700 {
-		mode := info.Mode().Perm()
-		return fmt.Errorf("%s is not owner-only: mode %04o, want 0700", dir, mode)
-	}
-	return nil
-}
-
-func usesPOSIXPermissions() bool {
-	return runtime.GOOS != "windows"
-}
-
 func openArchive(archivePath string) (*sql.DB, error) {
 	dsn, err := archiveDSN(archivePath, false)
 	if err != nil {
