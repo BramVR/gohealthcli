e75b0b1a3f5c71c1a4e80f65d6e38baf06590373
Bram Van Rompuy <bram.van.rompuy@gmail.com>
2026-06-16 19:13:22 +0200

    fix(credentials): preserve Windows file-store ACLs

--- cmd/gohealthcli/credential_store.go
+++ cmd/gohealthcli/credential_store.go
@@ -173,49 +173,6 @@ func (store fileCredentialStore) Store(key string, tokenMaterial map[string]any)
 	return writeCredentialStoreFile(store.path, content)
 }
 
-func writeCredentialStoreFile(path string, content []byte) error {
-	dir := filepath.Dir(path)
-	temp, err := os.CreateTemp(dir, "."+filepath.Base(path)+".tmp-*")
-	if err != nil {
-		return err
-	}
-	tempPath := temp.Name()
-	keepTemp := true
-	defer func() {
-		if keepTemp {
-			_ = os.Remove(tempPath)
-		}
-	}()
-	if usesPOSIXPermissions() {
-		if err := temp.Chmod(0o600); err != nil {
-			_ = temp.Close()
-			return err
-		}
-	}
-	if _, err := temp.Write(content); err != nil {
-		_ = temp.Close()
-		return err
-	}
-	if err := temp.Sync(); err != nil {
-		_ = temp.Close()
-		return err
-	}
-	if err := temp.Close(); err != nil {
-		return err
-	}
-	if err := rejectSymlinkCredentialStorePath(path); err != nil {
-		return err
-	}
-	if err := replaceCredentialStoreFile(tempPath, path); err != nil {
-		return err
-	}
-	keepTemp = false
-	if !usesPOSIXPermissions() {
-		return nil
-	}
-	return os.Chmod(path, 0o600)
-}
-
 func (store fileCredentialStore) Load(key string) (map[string]any, error) {
 	if err := rejectSymlinkCredentialStorePath(store.path); err != nil {
 		return nil, err
--- cmd/gohealthcli/credential_store_replace.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package main
-
-import "os"
-
-func replaceCredentialStoreFile(sourcePath, targetPath string) error {
-	return os.Rename(sourcePath, targetPath)
-}
